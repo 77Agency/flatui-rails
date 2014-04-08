7c96a5678e9cb4fc614017cc86539bd34261bd1b
Andrey Rukol <a.rukol@77agency.com>
2014-04-08 14:04:18 +0300

    Updated flat-ui

---
+++ vendor/assets/javascripts/flat-ui.js
@@ -0,0 +1,13 @@
+//= require jquery-ui-1.10.3.custom.min
+//= require jquery.ui.touch-punch.min
+//= require bootstrap.min
+//= require bootstrap-select
+//= require bootstrap-switch
+//= require flatui-checkbox
+//= require flatui-radio
+//= require jquery.tagsinput
+//= require jquery.placeholder
+//= require html5shiv
+//= require icon-font-ie7
+//= require respond.min
+//= require typeahead
